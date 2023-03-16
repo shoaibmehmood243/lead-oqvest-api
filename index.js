62ea86326315fabb0922197f91c3051a20620a1a
Shoaib Mehmood <shoaib.m@emblemtechnologies.com>
2023-03-16 16:06:02 +0500

    Error handling

--- index.js
+++ index.js
@@ -10,7 +10,25 @@ app.get('/', (req, res)=> {
     res.send('Lead oqvest api working...');
 });
 
-app.use('/form', homeRoute)
+app.use('/form', homeRoute);
+
+app.use((req, res, next)=>{
+    const err = new Error("Not found");
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next)=>{
+    if(err.isJoi) err.status = 422;
+    return res
+    .status(err.status || 500)
+    .send({
+        error: {
+            status : err.status || 500,
+            message : err.message
+        }
+    });
+})
 
 app.listen(3000, ()=> {
     console.log('Server listening on port 3000');
