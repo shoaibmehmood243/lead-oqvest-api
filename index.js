a78bd7d2865dc33b580ee06830e3198ca475351f
shoaibmehmood243 <shoaibmehmood065@gmail.com>
2023-06-20 01:35:07 +0500

    form submission

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const app = express();
 const homeRoute = require('./src/routes/home.route');
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: true,
     credentials: true
